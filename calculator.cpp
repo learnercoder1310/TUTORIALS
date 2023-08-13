af3932009d269f6c02099ffd34177e64ead004f8
learnercoder1310 <142147850+learnercoder1310@users.noreply.github.com>
2023-08-13 15:27:04 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    cout << "enter a";
+    cin >> a;
+    cout << "enter b";
+    cin >> b;
+    cout << a+b<< endl;
+    cout << a-b << endl;
+    cout << a%b << endl;
+    cout << a*b << endl;
+}
